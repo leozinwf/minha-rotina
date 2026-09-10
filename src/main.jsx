849e285d4e810a300e41b1222d691f54b1350c25
Leonardo <leoomorais@hotmail.com>
2026-09-10 09:55:08 -0300

    feat: require Firebase authentication before planner

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV6 from './AppV6'
+import AuthGate from './AuthGate'
 import { enableTimelineCurrentTime } from './timeline-now'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><AppV6 /></React.StrictMode>)
+createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthGate>
+      <AppV6 />
+    </AuthGate>
+  </React.StrictMode>
+)
 
 enableTimelineCurrentTime()
