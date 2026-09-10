2ac174c1d82f8ffb05fbbb1813e082b3ad85d582
Leonardo <leoomorais@hotmail.com>
2026-09-10 09:29:17 -0300

    feat: enable planner v5 interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import AppV4 from './AppV4'
-import './fixes.css'
+import AppV5 from './AppV5'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><AppV4 /></React.StrictMode>)
+createRoot(document.getElementById('root')).render(<React.StrictMode><AppV5 /></React.StrictMode>)
