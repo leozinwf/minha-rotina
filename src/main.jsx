c16caaef4a3ce29598c75b225750fe0b35b48d29
Leonardo <leoomorais@hotmail.com>
2026-09-10 09:45:07 -0300

    feat: center day timeline on current time

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV6 from './AppV6'
+import { enableTimelineCurrentTime } from './timeline-now'
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><AppV6 /></React.StrictMode>)
+
+enableTimelineCurrentTime()
