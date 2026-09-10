580059744012a27444f45387cce5de6fa6444953
Leonardo <leoomorais@hotmail.com>
2026-09-10 13:17:39 -0300

    feat: mount flexible task inbox

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV6 from './AppV6'
 import AuthGate from './AuthGate'
+import TaskInbox from './TaskInbox'
 import { enableTimelineCurrentTime } from './timeline-now'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <AuthGate>
       <AppV6 />
+      <TaskInbox />
     </AuthGate>
   </React.StrictMode>
 )
