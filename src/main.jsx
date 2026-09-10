0808c25c96233460d52efd39a4edffe6241e3a3f
Leonardo <leoomorais@hotmail.com>
2026-09-10 09:35:00 -0300

    feat: enable five minute timeline scale

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV5 from './AppV5'
+import './timeline-enhancer.css'
+import './timeline-enhancer'
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><AppV5 /></React.StrictMode>)
