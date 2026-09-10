a397abc2bdaeef94ad3877199884a87649cd3c0b
Leonardo <leoomorais@hotmail.com>
2026-09-09 21:39:22 -0300

    fix: load timeline and drag corrections

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import AppV4 from './AppV4'
+import './fixes.css'
 
 createRoot(document.getElementById('root')).render(<React.StrictMode><AppV4 /></React.StrictMode>)
