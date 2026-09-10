0d335f9508e93d73f26fee982fe8d86d415284ef
Leonardo <leoomorais@hotmail.com>
2026-09-09 21:06:31 -0300

    feat: enable improved planner interface

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-import './styles.css'
+import AppV4 from './AppV4'
 
-createRoot(document.getElementById('root')).render(<React.StrictMode><App /></React.StrictMode>)
+createRoot(document.getElementById('root')).render(<React.StrictMode><AppV4 /></React.StrictMode>)
